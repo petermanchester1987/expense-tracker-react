05ac6636d06743cf618217c546bd41a25790e826
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-08 18:53:44 +0100

    finished up using hooks and context api

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState.js";
+import uuid from "react-uuid";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: uuid(),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,19 +1,17 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState.js";
+import { Transaction } from "./Transaction.js";
 
 export const TransactionList = () => {
-  const context = useContext(GlobalContext);
-
-  console.log(context);
+  const { transactions } = useContext(GlobalContext);
 
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
