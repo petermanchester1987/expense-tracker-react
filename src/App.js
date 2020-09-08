14cd13147eee6f6485f0b41eeddeede796667dc3
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-08 17:00:37 +0100

    building out components and starting state and providers

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import "./App.css";
 import { Header } from "./components/Header.js";
 import { Balance } from "./components/Balance.js";
 import { IncomeExpenses } from "./components/IncomeExpenses.js";
+import { TransactionList } from "./components/TransactionList.js";
+import { AddTransaction } from "./components/AddTransaction.js";
+
+import { GlobalProvider } from "./context/GlobalState.js";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
